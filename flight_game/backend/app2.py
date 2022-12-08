e3342db07d2f7a6fdf31e36676fa485b12f87adc
samipaan <samipaan@metropolia.fi>
2022-12-08 16:04:10 +0200

    Version 2.0

---
+++ flight_game/backend/app2.py
@@ -0,0 +1,88 @@
+from flask import Flask, request
+from flask_cors import CORS
+#from game import Game
+from config import connection
+import json
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+def get_topten():
+    sql = f"SELECT * FROM top10 order by score desc"
+    query_cursor = connection.cursor()
+    query_cursor.execute(sql)
+    return query_cursor.fetchall()
+
+
+def newgame(userinput, location, transport, continent):
+    global g
+    g = Game(userinput, location, transport, continent)
+    data = g
+    json_data = json.dumps(data, default=lambda o: o.__dict__, indent=4)
+    return json_data
+
+
+def fly(location, prev_location, transport, continent):
+    g.player_status.update(location, prev_location, transport)
+    g.update(location, continent)
+    data = g
+    json_data = json.dumps(data, default=lambda o: o.__dict__, indent=4)
+    return json_data
+
+
+def refresh(location, continent, transport):
+    g.player_status.refresh(transport)
+    g.update(location, continent)
+    data = g
+    json_data = json.dumps(data, default=lambda o: o.__dict__, indent=4)
+    return json_data
+
+
+# http://127.0.0.1:5000/game?player=Vesa&loc=EFHK&continent=EU&transport=airplane
+@app.route('/game')
+def game():
+    args = request.args
+    player = args.get("player")
+    loc = args.get("loc")
+    transport = args.get('transport')
+    continent = args.get('continent')
+    data = json.load(open('../testdata/newgame.json'))
+    return data
+
+
+# http://127.0.0.1:5000/flyto?loc=EFHA&prevloc=EFHK&continent=EU&transport=airplane
+@app.route('/flyto')
+def flyto():
+    args = request.args
+    loc = args.get("loc")
+    prevloc = args.get("prevloc")
+    transport = args.get('transport')
+    continent = args.get('continent')
+    data = json.load(open('../testdata/fly.json'))
+    return data
+
+
+# http://127.0.0.1:5000/refresh?loc=EFHKcontinent=EU&transport=airplane
+@app.route('/refresh')
+def refresh_game():
+    args = request.args
+    loc = args.get("loc")
+    transport = args.get('transport')
+    continent = args.get('continent')
+    data = json.load(open('../testdata/refresh.json'))
+    return data
+
+
+# http://127.0.0.1:5000/topten
+@app.route('/topten')
+def get_topten_data():
+    data = get_topten()
+    json_data = json.dumps(data, default=lambda o: o.__dict__, indent=4)
+    return json_data
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
---
+++ flight_game/js/main2.js
@@ -0,0 +1,298 @@
+'use strict';
+
+/* 1. show map using Leaflet library. (L comes from the Leaflet library) */
+
+const map = L.map('map', {tap: false});
+L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(map);
+map.setView([60, 24], 7);
+
+
+// global variables
+let number = 0;
+const apiurl = 'http://127.0.0.1:5000/';
+
+const globalGoals = [];
+const airportMarkers = L.featureGroup().addTo(map);
+const playerLocation = L.marker([0, 0]);
+let playerLoc = 'EFHK'
+let continent = 'EU'
+let transport = 'airplane'
+
+// icons
+const blueIcon = L.divIcon({className: 'blue-icon'});
+
+const bolivarIcon = L.icon({
+    iconUrl: 'img/bolivar_airplane.png',
+    iconSize:     [120, 90], // size of the icon
+    iconAnchor:   [60, 45], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-25, -50] // point from which the popup should open relative to the iconAnchor
+});
+
+// form for player name
+document.querySelector('#player-form').addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    const playerName = document.querySelector('#player-input').value;
+    document.querySelector('#player-modal').classList.add('hide');
+    gameSetup(`${apiurl}game?player=${playerName}&loc=${playerLoc}&continent=${continent}&transport=${transport}`);
+})
+
+// function to fetch data from API
+async function getData(url) {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Invalid server input!');
+    const data = await response.json();
+    return data
+}
+
+// function to update game status
+function updateStatus(status) {
+    document.querySelector('#player-name').innerHTML = `Player: ${status.name}`;
+    document.querySelector('#score').innerHTML = status.score;
+    document.querySelector('#budget').innerHTML = status.co2_budget;
+    document.querySelector('#distance').innerHTML = status.distance.toFixed(1);
+    document.querySelector('#time').innerHTML = status.travel_time.toFixed(1);
+    // Should we add here more data about player's current status? Like distance? Time?
+}
+
+// function to show weather at selected airport
+function showWeather(data) {
+    document.querySelector('#airport-name').innerHTML = `${data.location.name}`;
+    document.querySelector('#airport-temp').innerHTML = `${data.weather.temp}°C`;
+    document.querySelector('#weather-icon').src = data.weather.icon;
+    document.querySelector('#airport-conditions').innerHTML = data.weather.description;
+    document.querySelector('#wind').innerHTML = `${data.weather.wind.speed}m/s`;
+}
+
+// function to check if any goals have been reached
+function checkGoals(playerGoals) {
+    if (playerGoals.length > 0) {
+        for (let goal of playerGoals) {
+            if (!globalGoals.includes(goal.goalid)) {
+                document.querySelector('.goal').classList.remove('hide');
+            }
+        }
+    }
+}
+
+// function to update goal data and goal table in UI
+function updateGoals(goals) {
+    document.querySelector('#goals').innerHTML = '';
+    for (let goal of goals) {
+        const div = document.createElement('div');
+        div.classList.add('goal-item');
+        const img = document.createElement("img");
+        img.src = goal.icon;
+        img.alt = `goal name: ${goal.name}`;
+        const p = document.createElement('p');
+        p.innerHTML = goal.description;
+        div.appendChild(img)
+        div.appendChild(p)
+        if (goal.reached) {
+            div.classList.add('done');
+            globalGoals.includes(goal.goalid) || globalGoals.push(goal.goalid);
+        }
+        document.querySelector('#goals').append(div)
+    }
+}
+
+// function to check if game is over
+function checkGameOver(data) {
+    if (data.player_status.co2_budget <= 0) {
+        alert(`Game Over. ${globalGoals.length} goals reached.`);
+        if (data.player_status.to_topten) {
+            build_top_ten();
+            const toptenModal = document.querySelector('#topten-modal');
+            const p = document.createElement("p");
+            p.innerHTML = 'NICE FLYING THERE!! U MADE IT TO THE TOP10!!';
+            toptenModal.appendChild(p);
+            toptenModal.classList.remove('hide');
+        }
+        return false;
+    } else if (data.player_status.goals.length >= 8) {
+        alert('You won the game');
+        if (data.player_status.to_topten) {
+            build_top_ten();
+            const toptenModal = document.querySelector('#topten-modal');
+            const p = document.createElement("p");
+            p.innerHTML = 'NICE FLYING THERE!! U MADE IT TO THE TOP10!!';
+            toptenModal.appendChild(p);
+            toptenModal.classList.remove('hide');
+        }
+    } else {
+        return true;
+    }
+}
+
+// function to set all buttons to default style
+function clearButtons(target, style1, style2) {
+    const elements = document.getElementsByClassName(`${target}`)
+    for (let e of elements) {
+        e.classList.replace(style1, style2);
+    }
+}
+
+// function to build topten10 table
+async function build_top_ten() {
+    const data = await getData(`${apiurl}topten`);
+    let i = 1;
+    const table = document.querySelector('#topten-table');
+    table.innerHTML = '';
+    const tr3 = document.createElement("tr");
+    const td3 = document.createElement("td");
+    td3.innerHTML = '#';
+    tr3.appendChild(td3);
+    const td4 = document.createElement("td");
+    td4.innerHTML = 'Name';
+    tr3.appendChild(td4);
+    const td5 = document.createElement("td");
+    td5.innerHTML = 'Score';
+    tr3.appendChild(td5);
+    table.appendChild(tr3)
+
+    for (let player of data) {
+        const tr = document.createElement("tr");
+        const placeNumber = document.createElement('td');
+        placeNumber.innerHTML = i.toString();
+        tr.appendChild(placeNumber);
+        const td = document.createElement("td");
+        td.innerHTML = player[1];
+        tr.appendChild(td);
+        const td2 = document.createElement("td");
+        td2.innerHTML = player[2];
+        tr.appendChild(td2);
+        table.appendChild(tr);
+        i++;
+    }
+    const toptenModal = document.querySelector('#topten-modal')
+    document.querySelector('#topten-button').addEventListener('click', function () {
+        toptenModal.classList.remove('hide');
+        location.href = "#top";
+    });
+}
+
+// function to wait for a while
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// function to move marker to target coordinates
+async function moveMarker(data) {
+    if (data.player_status.flight_coords.length > 1) {
+        map.flyTo([data.continent[0], data.continent[1]], 3);
+        await sleep(3000);
+        for (let coordinates of data.player_status.flight_coords) {
+            map.flyTo([coordinates[0], coordinates[1]], 5);
+            playerLocation.setLatLng([coordinates[0], coordinates[1]]);
+            await sleep(25);
+        }
+    } else {
+        playerLocation.setLatLng([data.location.latitude, data.location.longitude]);
+        playerLocation.setIcon(bolivarIcon);
+        playerLocation.addTo(map);
+        map.flyTo([data.location.latitude, data.location.longitude]);
+        playerLocation.setIcon(bolivarIcon);
+        playerLocation.openPopup();
+        map.flyTo([data.continent[0], data.continent[1]], 3);
+    }
+    playerLocation.bindPopup(`You are here: <b>${data.location.name}</b>`);
+    playerLocation.openPopup();
+
+}
+
+function tranportButtons() {
+    const buttons = document.querySelectorAll(".button-transport");
+    for (let button of buttons) {
+        button.addEventListener(`click`, function (evt) {
+            buttons.forEach(i => i.classList.remove('active'));
+            transport = evt.currentTarget.value;
+            this.classList.toggle('active');
+        });
+    }
+}
+
+function continentButtons() {
+    const buttons = document.querySelectorAll(".button-world");
+    for (let button of buttons) {
+        button.addEventListener(`click`, function (evt) {
+            buttons.forEach(i => i.classList.remove('active'));
+            continent = evt.currentTarget.value;
+            this.classList.toggle('active');
+        });
+    }
+}
+
+// function to set up game
+// this is the main function that creates the game and calls the other functions
+async function gameSetup(url) {
+    try {
+        document.querySelector('.goal').classList.add('hide');
+        airportMarkers.clearLayers();
+        const gameData = await getData(url);
+        console.log(gameData);
+        playerLoc = gameData.location.ident;
+        continent = gameData.continent[2];
+
+        continentButtons();
+        tranportButtons();
+        if (!checkGameOver(gameData)) return
+        await moveMarker(gameData);
+        updateStatus(gameData.player_status);
+        showWeather(gameData);
+
+        if (gameData.airports.length < 1) {
+            alert("No airports found. Remember: Landing spots for balloon are only found in North-America and some in Europe.");
+        } else {
+            for (let airport of gameData.airports) {
+                const marker = L.marker([airport.latitude, airport.longitude]).addTo(map);
+                airportMarkers.addLayer(marker)
+                marker.setIcon(blueIcon);
+                const popupContent = document.createElement("div");
+                const h4 = document.createElement('h4');
+                h4.innerHTML = airport.name;
+                popupContent.append(h4);
+                const goButton = document.createElement("button");
+                goButton.classList.add('button');
+                goButton.innerHTML = 'Fly here';
+                popupContent.append(goButton);
+                const p = document.createElement("p");
+                p.innerHTML = `Distance ${airport.distance}km`;
+                popupContent.append(p);
+                marker.bindPopup(popupContent);
+                goButton.addEventListener('click', function () {
+                    gameSetup(`${apiurl}flyto?loc=${airport.ident}&prevloc=${playerLoc}&continent=${continent}&transport=${transport}`);
+                });
+            }
+        }
+        checkGoals(gameData.player_status.goals);
+        updateGoals(gameData.goals);
+    } catch
+        (error) {
+        console.log(error);
+    }
+}
+
+// event listener to hide goal splash
+document.querySelector('.goal').addEventListener('click', function (evt) {
+    evt.currentTarget.classList.add('hide');
+});
+
+// event listener for search button
+document.querySelector('#search-button').addEventListener('click', function () {
+    gameSetup(`${apiurl}refresh?loc=${playerLoc}&continent=${continent}&transport=${transport}`)
+});
+
+// event listener for topten
+document.querySelector('#topten-button').addEventListener('click', build_top_ten())
+document.querySelector('#topten-close').addEventListener('click', function () {
+    document.querySelector('#topten-modal').classList.add('hide');
+});
+
+
+
+
+
+
+
